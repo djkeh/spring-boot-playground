2e4456741b9631680fd58d730f35f1ec6d9d60ff
Uno Kim <djkehh@gmail.com>
2022-08-14 22:15:02 +0900

    boot 프로젝트 셋업
    부트 2.7.2
    
    actuator + jpa + thymeleaf + web + lombok + devtools + configuration processor + h2
    
    편리한 디버그를 위해 미리 `properties.yaml` 기초 세팅도 해둠

---
+++ src/main/java/com/example/springbootplayground/SpringBootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
